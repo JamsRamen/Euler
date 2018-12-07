d1f67b872a0439c1b6026e038117a6f2bc0bce59
James Rayman <jamsramen@gmail.com>
2018-12-06 21:24:46 -0600

    Adds functions

--- euler.py
+++ euler.py
@@ -1,6 +1,7 @@
 import math
 
 # number theory
+
 def gcd(a, b):
     if a < 0:
         a = -a;
@@ -30,5 +31,69 @@ def sieve(n):
             primes.append(i)
     
     return isPrime, primes
-            
-            
+
+def modpow (b, e, m):
+    r = 1
+    while e > 0:
+        if e % 2 == 1:
+            r = r*b % m
+        b = b*b % m
+        e //= 2
+    return r
+
+# run Miller-Rabin primality test k times
+def probablePrime(n, k):
+    pass
+
+# return the exponents of the prime factorization of n
+def factorize(n):
+    pass
+    
+# return a sorted list of the divisors of n
+def divisors(n):
+    pass
+    
+def sigma(n, d):
+    pass
+    
+def totient(n):
+    pass
+
+def mobius(n):
+    pass
+
+    
+    
+# combinatorics
+
+def factorial(n):
+    pass
+
+def choose(n, r):
+    pass
+
+def permute(n, r):
+    pass
+
+
+# computer science
+
+# note: nextPermutation does not work with strings
+def nextPermutation(v):
+    i = len(v) - 1
+    while i > 0 and v[i-1] >= v[i]:
+        i = i - 1
+    if i <= 0:
+        return False
+
+    j = len(v) - 1
+    while v[j] <= v[i -1]:
+        j = j - 1
+    v[i-1],v[j] = v[j],v[i-1]
+
+    v[i:] = v[len(v) - 1 : i-1 : -1]
+    ans = [chr(i) for i in v]
+    v = "".join(ans)
+    return True
+
+
